8cdf4842b984b00fb8d123a2f674731d87a14ad9
SEBO3 <SEBO@simcorp.com>
2023-02-17 15:19:51 +0200

    New 1

---
+++ MyFirstApp/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Class2    
+    {
+        Console.WriteLine("Hello Sergii!");
+    }
+}
